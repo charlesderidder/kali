0cee28a2e12f8e5d0bc06fca1b0e3644df0cee0e
charlesderidder <51724441+charlesderidder@users.noreply.github.com>
2022-04-26 22:08:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@
 # First run
 #sudo apt update && sudo apt -y upgrade && sudo apt -y dist-upgrade && sudo apt -y autoremove && sudo apt autoclean
 sudo apt update
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
+sudo apt autoremove -y
+sudo apt autoclean -y
 
 
 # Run next time only enter update in commandline
