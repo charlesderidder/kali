3f40907d8e1f224307ebd649c2577fce7f6c9dd8
charlesderidder <51724441+charlesderidder@users.noreply.github.com>
2022-04-26 16:18:07 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,2 @@
+# update Kali door "update" in command line in te typen
+# sudo apt update && sudo apt -y upgrade && sudo apt -y dist-upgrade && sudo apt -y autoremove && sudo apt autoclean > update && sudo mv update /usr/local/bin/update && sudo chmod +x /usr/local/bin/update
