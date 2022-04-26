9924f330f029f23da866d67d44220925f30a8ebb
charlesderidder <51724441+charlesderidder@users.noreply.github.com>
2022-04-26 16:42:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 # sudo apt update
 # echo ""
 # Will show your public facing IP Address
-echo "WAN IP Address is:" 
-dig +short myip.opendns.com @resolver1.opendns.com
+# echo "WAN IP Address is:" 
+# dig +short myip.opendns.com @resolver1.opendns.com
 
 
 # First run
