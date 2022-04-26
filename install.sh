4983371b3ae2c9a29f1f53a229f9d645c23215dd
charlesderidder <51724441+charlesderidder@users.noreply.github.com>
2022-04-26 21:51:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ echo "sudo apt update && sudo apt -y upgrade && sudo apt -y dist-upgrade && sudo
 sudo apt update sudo apt install -y docker.io sudo systemctl enable docker --now sudo usermod -aG docker charlesderidder
 
 # Install Portainer // CHANGE PORT
-docker volume create portainer_data docker run -d -p 8000:8000 -p 9443:9443 --name portainer \ --restart=always \ -v /var/run/docker.sock:/var/run/docker.sock \ -v portainer_data:/data \ portainer/portainer-ce:2.9.3
+# docker volume create portainer_data docker run -d -p 8000:8000 -p 9443:9443 --name portainer \ --restart=always \ -v /var/run/docker.sock:/var/run/docker.sock \ -v portainer_data:/data \ portainer/portainer-ce:2.9.3
 
 reset
 alias cls='printf "\033c"'
