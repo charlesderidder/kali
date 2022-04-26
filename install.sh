1fa6b66431cd4b8a270eecaa5db1eb0408fba243
charlesderidder <51724441+charlesderidder@users.noreply.github.com>
2022-04-26 16:48:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,3 @@
-# update Kali door "update" in command line in te typen
-# sudo apt update && sudo apt -y upgrade && sudo apt -y dist-upgrade && sudo apt -y autoremove && sudo apt autoclean > update && sudo mv update /usr/local/bin/update && sudo chmod +x /usr/local/bin/update
-
-# sudo apt update
-# echo ""
-# Will show your public facing IP Address
-# echo "WAN IP Address is:" 
-# dig +short myip.opendns.com @resolver1.opendns.com
-
-
 # First run
 sudo apt update && sudo apt -y upgrade && sudo apt -y dist-upgrade && sudo apt -y autoremove && sudo apt autoclean
 
